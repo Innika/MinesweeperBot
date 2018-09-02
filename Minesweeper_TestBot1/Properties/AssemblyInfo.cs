f3026e2a0ea48b110365bd1ec5c665ef4762baac
innikaart <innika4444@gmail.com>
2018-09-02 13:03:59 +0200

    Navidating to http://minesweeperonline.com

---
+++ Minesweeper_TestBot1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Minesweeper_TestBot1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Minesweeper_TestBot1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce72f183-350a-4cfd-816c-41bbb6ab5274")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Minesweeper_TestBot1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Minesweeper_TestBot1
+{
+    [TestClass]
+    public class Minesweeper
+    {
+        IWebDriver drv = new ChromeDriver();
+        IWebElement square(int x, int y) => drv.FindElement(By.Id(string.Format("{0}_{1}", x, y)));
+
+        [TestMethod]
+        public void PlayMinesweeper()
+        {
+            drv.Navigate().GoToUrl("http://minesweeperonline.com/");
+
+            Random rnd = new Random();
+            int x = rnd.Next(1, 30);
+            int y = rnd.Next(1, 16);
+
+            square(x, y).Click();
+
+        }        
+    }
+}
